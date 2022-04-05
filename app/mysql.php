f4528005fc4c2bdae3fac7238da29a7affc715ca
Chudomaster <1903007@uadeo.mx>
2022-04-05 09:11:27 -0600

    C13

--- app/mysql.php
+++ app/mysql.php
@@ -1,3 +1,12 @@
 <?php 
-    //TODO: conexion de mysql
+   use \mysqli;
+   class Mysql extends \mysqli {
+       protected $data = [
+           'host' => '127.0.0.1',
+           'user' => 'root',
+           'password' => '',
+           'dbname' => 'uadeo',
+           'port' => 3306
+       ];
+   }
 ?>
